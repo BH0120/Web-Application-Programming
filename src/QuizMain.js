f2951fa0b7383071af68d89dff5666e500020091
BH0120 <kbh9757@naver.com>
2024-06-09 17:49:27 +0900

    폰트 추가(임시), padding 수치 변경

--- src/QuizMain.js
+++ src/QuizMain.js
@@ -9,7 +9,7 @@ function QuizMain() {
     <div className="QuizMain">
       <header className="QuizMain-header">
         <h1 className="QuizMain-title">인지 중재 치료</h1>
-        <p className='QuizMian-title'>치매 환자의 인지 기능 향상과 일반인들의 치매 예방을 위한 두뇌 훈련 웹사이트</p>
+        <p className=''>치매 환자의 인지 기능 향상과 일반인들의 치매 예방을 위한 두뇌 훈련 웹사이트</p>
         <p className="QuizMain-info">20191122 고병현</p>
       </header>
       <main className="QuizMain-main">
